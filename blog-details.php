906ede2755d45dffc5181c18efacd848fce5400a
Ayman H <ayman.s.hamdoun@gmail.com>
2018-04-19 00:39:10 +0300

    Initial Structure + Blog View

---
+++ blog-details.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ecospots | Blog</title> 
+
+	<!-- mobile responsive meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/responsive.css">
+
+	<link rel="apple-touch-icon" sizes="180x180" href="images/fav-icon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" href="images/fav-icon/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="images/fav-icon/favicon-16x16.png" sizes="16x16">
+
+ 
+
+</head>
+<body>
+
+<div class="boxed_wrapper">
+
+<?php require_once 'elements/header.php';?> 
+
+<div class="inner-banner has-base-color-overlay text-center" style="background: url(images/background/4.jpg);">
+    <div class="container">
+        <div class="box">
+            <h1>Blog Single Post</h1>
+        </div>
+    </div>
+</div>
+
+
+<section class="blog-single-post blog-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9 col-md-offset-2 col-sm-12">
+                <div class="breadcumb-wrapper">
+                    <div class="container">
+                        <div class="center-block">
+                            <ul class="list-inline link-list">
+                                <li>
+                                    <a href="index.html">Home</a>
+                                </li>
+                                <li>
+                                    <a href="blog-1.html">blog</a>
+                                </li>
+                                
+                                <li>
+                                    Blog Single Post
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="post-area">
+                    <article class="default-blog-news">
+                        <figure class="img-holder">
+                            <img src="images/blog/l1.jpg" alt="News">
+                        </figure>
+                        <div class="lower-content">
+                            <div class="date">March 02, 2017</div>
+                                                        
+                            <h4>My Zero waste</h4>
+                        </div>
+                    </article>
+                    <div class="content-box">
+                        <div class="text">
+                            <p>We denounce with righteous indignation and dislike men who are beguiled and seds demoralized by the charm pleasure of the moment,blinded by desire, that they cannot foresee the pains ut and trouble that are bound to ensue and equal blame belongs to those who fail in their duty through weakness of will, uts  from toil and painare perfectly simple and easy to distinguish. </p> 
+                            <p>Pleasures and praising pain was born and will give you a completed account of the systems seds expound the actual teaching of the great explorer the master builder nor again there anyones who loves or pursues desires to obtain pain of itself, because it is pain, but because occasionally.</p>              
+                        
+                            <p>Know how to pursue pleasure rationally encounter consequences that are extremely painful seds or again is there anyone who loves or pursues or desires to obtain pain of itself, because seds our it pain, but because occasionally circumstances occur which toil and pain can  undertakes laborious physical exercise.</p>
+                        </div>
+                        <div class="outer-box">
+
+                            <div class="share-box clearfix">
+                                <ul class="tag-box pull-left">
+                                    <li>Category: </li>
+                                    <li><a href="#">Waste,</a></li>
+                                    <li><a href="#">Recycling,</a></li>
+                                    <li><a href="#">Energy,</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="post-author">
+                                <div class="inner-box">
+                                    <figure class="author-thumb"><img src="images/blog/author2.jpg" alt=""></figure>
+                                    <h4>Mark Richardson</h4>
+                                </div>
+                            </div>
+                            
+                        </div>
+
+                    </div>
+
+                </div>
+                    
+            </div>            
+        </div>
+    </div>
+</section>
+
+
+    <?php require_once 'elements/footer.php';?>
+
+    <!-- Scroll Top  -->
+    <button class="scroll-top tran3s color2_bg"><span class="fa fa-angle-up"></span></button>
+    <!-- preloader  -->
+    <div class="preloader"></div>
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/menu.js"></script>
+    <script src="js/imagezoom.js"></script> 
+    <script src="js/SmoothScroll.js"></script>
+    <script src="js/pie-chart.js"></script> 
+    <script src="js/custom.js"></script>
+
+</div>
+    
+</body>
+</html>
